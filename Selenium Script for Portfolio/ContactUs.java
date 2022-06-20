8a7b348cf6ccf8c2f624ed4264212ccd4f9173fd
Rahul Kumar <106374111+Rahulshah1256@users.noreply.github.com>
2022-06-20 14:35:45 +0530

    Add files via upload

---
+++ Selenium Script for Portfolio/ContactUs.java
@@ -0,0 +1,29 @@
+package rahulSelenium;
+import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+
+public class ContactUs {
+
+	public static void main(String[] args) {
+		/*System.setProperty("webdriver.chrome.driver","C:\\Users\\itctesting27\\Downloads\\Selenium\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://selenium.dev");
+		driver.close();*/
+		
+		String path="C:\\Users\\itctesting27\\Downloads\\Selenium\\chromedriver_win32\\chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", path);
+		WebDriver driver=new ChromeDriver();
+		
+		
+		driver.get("file:///C:/Users/itctesting27/Downloads/Portfolio-WebApplication/Registration%20Form/Login%20Form/Home%20Page/rahulPortfolio/index.html");
+		driver.findElement(By.id("name")).sendKeys("rahul");
+		driver.findElement(By.id("email")).sendKeys("rahul@gmail.com");
+		driver.findElement(By.id("subject")).sendKeys("Send your Resume");
+		driver.findElement(By.id("message")).sendKeys("I'am quite impressed with your portfolio. May I know what is the good time to talk with you?");
+		driver.findElement(By.id("submit")).click();
+	}
+
+}
